dc4a93c97fb4ff4b0e6adadb22cbdda54a84b508
Philiks <janopol.felixjr@gmail.com>
2022-09-20 16:00:47 +0800

    [Changed] depracated `on_server_ready()`. Apply changes stated in williamboman/nvim-lsp-installer#636

--- nvim/lua/user/lsp/lsp-installer.lua
+++ nvim/lua/user/lsp/lsp-installer.lua
@@ -1,29 +1,39 @@
-local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
-if not status_ok then
+local lsp_installer_status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not lsp_installer_status_ok then
   vim.notify("Failed to load package: 'nvim-lsp-installer'")
-	return
+  return
 end
 
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
-lsp_installer.on_server_ready(function(server)
-	local opts = {
-		on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilities,
-	}
+lsp_installer.setup {}
 
-	 if server.name == "jsonls" then
-	 	local jsonls_opts = require("user.lsp.settings.jsonls")
-	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	 end
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+  vim.notify("Failed to load package: 'lspconfig'")
+  return
+end
 
-	 if server.name == "sumneko_lua" then
-	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-	 end
+-- `on_server_ready` is deprecated, use individual `setup` for each server.
+-- See: https://github.com/williamboman/nvim-lsp-installer/discussions/636
+local opts = {
+  on_attach = require("user.lsp.handlers").on_attach,
+  capabilities = require("user.lsp.handlers").capabilities,
+}
 
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	server:setup(opts)
-end)
+lspconfig.sumneko_lua.setup {
+  on_attach = opts.on_attach,
+  capabilities = opts.capabilities,
+  require("user.lsp.settings.sumneko_lua")
+}
 
+lspconfig.jsonls.setup {
+  on_attach = opts.on_attach,
+  capabilities = opts.capabilities,
+  require("user.lsp.settings.jsonls")
+}
+
+for _, server in ipairs { "tsserver", "jdtls", "html" } do
+  lspconfig[server].setup {
+    on_attach = opts.on_attach,
+    capabilities = opts.capabilities,
+  }
+end
